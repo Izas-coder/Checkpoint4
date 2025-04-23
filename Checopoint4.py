80b831686a004b157b274acd56585ebea3f2dcd5
Izaskun Salazar <izaskunsalazar72@gmail.com>
2025-04-23 16:35:48 +0200

    Update Checopoint4.py

--- Checopoint4.py
+++ Checopoint4.py
@@ -19,20 +19,28 @@ primer_valor=list(primero.items())[0]
 print(primer_valor)
 
 #Exercise 5: Select the second element from your tuple.
+
 segundo= exercise_one[0]
 segundo_valor=segundo[1]
 print(segundo_valor)
+
 #Exercise 6: Add an element to the end of your list.
+
 exercise_one += ['four']
 print(exercise_one)
+
 #Exercise 7: Replace the first element in your list.
+
 exercise_one[0] =  ('uno', 'dos', 'tres')
 print(exercise_one)
+
 #Exercise 8: Sort your list alphabetically.
 #I have created a list for this exercise because in exercise_one the list I have created includes a tuple, a float... and I can`t sort it alphabetically.
+
 exercise_eight = ['uno', 'dos', 'tres', 'cuatro']
 exercise_eight.sort()
 print(exercise_eight)
+
 #Exercise 9: Use reassignment to add an element to your tuple.
 exercise_nine = exercise_one[0]
 exercise_nine += ('cuatro',)
